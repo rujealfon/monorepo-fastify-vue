dff2047fd0513e30f5244d027f6e6dbd23d2eaf1
Ruje Alfon <rujealfon@users.noreply.github.com>
2026-07-09 21:29:48 +0800

    fix: update output directory for Vite build and Vercel configuration

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: "../api/public",
+    outDir: "../../dist",
     emptyOutDir: true,
   },
   resolve: {
